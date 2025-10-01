159b569d666ab13138f08591732755454c18b3b3
lorenzosalti <lorenzosalti@gmail.com>
2025-10-01 22:14:33 +0200

    fix: correct import of Record

--- esao-backoffice/src/main/java/org/lessons/java/esao_backoffice/repository/RecordRepository.java
+++ esao-backoffice/src/main/java/org/lessons/java/esao_backoffice/repository/RecordRepository.java
@@ -1,6 +1,7 @@
 package org.lessons.java.esao_backoffice.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.lessons.java.esao_backoffice.model.Record;
 
 public interface RecordRepository extends JpaRepository<Record, Integer> {
 
