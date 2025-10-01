006e827328475105f7e1aff844d64bf1979f286d
lorenzosalti <lorenzosalti@gmail.com>
2025-10-01 20:47:31 +0200

    initialize backoffice Java Spring Boot project

---
+++ esao-backoffice/src/main/java/org/lessons/java/esao_backoffice/EsaoBackofficeApplication.java
@@ -0,0 +1,13 @@
+package org.lessons.java.esao_backoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsaoBackofficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsaoBackofficeApplication.class, args);
+	}
+
+}
---
+++ esao-backoffice/src/test/java/org/lessons/java/esao_backoffice/EsaoBackofficeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lessons.java.esao_backoffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsaoBackofficeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
