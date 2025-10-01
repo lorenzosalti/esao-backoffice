e1ac9a8de0d5e0b13cc26d55fbd461100d825b05
lorenzosalti <lorenzosalti@gmail.com>
2025-10-01 21:18:05 +0200

    chore: create RecordRepository

---
+++ esao-backoffice/src/main/java/org/lessons/java/esao_backoffice/repository/RecordRepository.java
@@ -0,0 +1,7 @@
+package org.lessons.java.esao_backoffice.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RecordRepository extends JpaRepository<Record, Integer> {
+
+}
