d7a39611fabb06cce2f85e7b89a0732165f8c09a
lorenzosalti <lorenzosalti@gmail.com>
2025-10-02 16:54:19 +0200

    chore: create ConditionRepository

---
+++ esao-backoffice/src/main/java/org/lessons/java/esao_backoffice/repository/ConditionRepository.java
@@ -0,0 +1,8 @@
+package org.lessons.java.esao_backoffice.repository;
+
+import org.lessons.java.esao_backoffice.model.Condition;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ConditionRepository extends JpaRepository<Condition, Integer> {
+
+}
