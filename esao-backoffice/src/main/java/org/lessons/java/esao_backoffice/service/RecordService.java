3e6fcecf71a796cdf18695c108160f16128a1364
lorenzosalti <lorenzosalti@gmail.com>
2025-10-03 15:02:30 +0200

    chore: create RecordService

---
+++ esao-backoffice/src/main/java/org/lessons/java/esao_backoffice/service/RecordService.java
@@ -0,0 +1,47 @@
+package org.lessons.java.esao_backoffice.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.lessons.java.esao_backoffice.model.Record;
+import org.lessons.java.esao_backoffice.repository.RecordRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RecordService {
+
+  @Autowired
+  private RecordRepository recordRepository;
+
+  public List<Record> findAll() {
+    return recordRepository.findAll();
+  }
+
+  public Optional<Record> findById(Integer id) {
+    return recordRepository.findById(id);
+  }
+
+  public Record getById(Integer id) {
+    Optional<Record> recordAttempt = recordRepository.findById(id);
+    return recordAttempt.get();
+  }
+
+  public void create(Record recordToCreate) {
+    recordRepository.save(recordToCreate);
+  }
+
+  public void edit(Record recordToEdit) {
+    recordRepository.save(recordToEdit);
+  }
+
+  public void delete(Record recordToDelete) {
+    recordRepository.delete(recordToDelete);
+  }
+
+  public void deleteById(Integer id) {
+    Optional<Record> recordAttempt = recordRepository.findById(id);
+    recordRepository.delete(recordAttempt.get());
+  }
+
+}
