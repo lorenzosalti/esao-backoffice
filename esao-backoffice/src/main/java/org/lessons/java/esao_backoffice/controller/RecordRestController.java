6ecfd8f97d9ce85824f2c04250b8d3d6c8913c74
lorenzosalti <lorenzosalti@gmail.com>
2025-10-06 16:31:02 +0200

    feat: create RecordRestController

---
+++ esao-backoffice/src/main/java/org/lessons/java/esao_backoffice/controller/RecordRestController.java
@@ -0,0 +1,39 @@
+package org.lessons.java.esao_backoffice.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.lessons.java.esao_backoffice.model.Record;
+import org.lessons.java.esao_backoffice.service.RecordService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/records")
+public class RecordRestController {
+
+  @Autowired
+  private RecordService recordService;
+
+  @GetMapping
+  public ResponseEntity<List<Record>> index() {
+    return new ResponseEntity<List<Record>>(recordService.findAll(), HttpStatus.OK);
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<Record> show(@PathVariable Integer id) {
+    Optional<Record> recordAttempt = recordService.findById(id);
+    if (recordAttempt.isEmpty()) {
+      return new ResponseEntity<Record>(HttpStatus.NOT_FOUND);
+    }
+    return new ResponseEntity<Record>(recordService.getById(id), HttpStatus.OK);
+  }
+
+}
