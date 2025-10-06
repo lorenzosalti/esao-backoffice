f8115d988b0c2b5028a35ca01512ec7f2c70544e
lorenzosalti <lorenzosalti@gmail.com>
2025-10-06 12:30:53 +0200

    chore: roles type from List to Set

--- esao-backoffice/src/main/java/org/lessons/java/esao_backoffice/model/User.java
+++ esao-backoffice/src/main/java/org/lessons/java/esao_backoffice/model/User.java
@@ -1,6 +1,6 @@
 package org.lessons.java.esao_backoffice.model;
 
-import java.util.List;
+import java.util.Set;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -32,7 +32,7 @@ public class User {
 
   @ManyToMany(fetch = FetchType.EAGER)
   @JoinTable(name = "role_user", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
-  private List<Role> roles;
+  private Set<Role> roles;
 
   public User() {
   }
@@ -61,11 +61,11 @@ public class User {
     this.password = password;
   }
 
-  public List<Role> getRoles() {
+  public Set<Role> getRoles() {
     return roles;
   }
 
-  public void setRoles(List<Role> roles) {
+  public void setRoles(Set<Role> roles) {
     this.roles = roles;
   }
 
