6504e823f5e74af1afdb4775cb86effce9348848
lorenzosalti <lorenzosalti@gmail.com>
2025-10-06 10:26:25 +0200

    chore: add abstract method findByUsername

--- esao-backoffice/src/main/java/org/lessons/java/esao_backoffice/repository/UserRepository.java
+++ esao-backoffice/src/main/java/org/lessons/java/esao_backoffice/repository/UserRepository.java
@@ -1,8 +1,10 @@
 package org.lessons.java.esao_backoffice.repository;
 
+import java.util.Optional;
+
 import org.lessons.java.esao_backoffice.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-
+  Optional<User> findByUsername(String username);
 }
