89bdfbf5e5af0082b9e73cb8388788682d405532
lorenzosalti <lorenzosalti@gmail.com>
2025-10-07 19:19:20 +0200

    feat: create Loading

--- esao-frontend/src/layouts/DefaultLayout.jsx
+++ esao-frontend/src/layouts/DefaultLayout.jsx
@@ -1,14 +1,20 @@
 import { Outlet } from "react-router-dom"
 import Header from "../components/Header"
 import Footer from "../components/Footer"
+import { useContext } from "react"
+import GlobalContext from "../contexts/GlobalContext"
+import Loading from "../components/Loading"
 
 
 function DefaultLayout() {
+
+  const { isLoading } = useContext(GlobalContext)
 
   return (
     <>
       <Header />
       <main>
+        {isLoading && <Loading />}
         <Outlet />
       </main>
       <Footer />
