a302c0e614c2f187b134dccb851f0447b04232b1
lorenzosalti <lorenzosalti@gmail.com>
2025-10-07 16:30:06 +0200

    feat: add length check on records

--- esao-frontend/src/pages/Records.jsx
+++ esao-frontend/src/pages/Records.jsx
@@ -8,9 +8,9 @@ function Records() {
       <div className="container text-center">
         <div className="row row-cols-3 gy-2">
 
-          {records.map(record => (
+          {records.length() ? records.map(record => (
             <RecordCard key={record.id} data={record} />
-          ))}
+          )) : <h2>404 Nessun disco trovato</h2>}
 
         </div>
       </div>
