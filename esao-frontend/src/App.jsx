4e916afdc163f24bc25d17845a0fc81814e4d5b8
lorenzosalti <lorenzosalti@gmail.com>
2025-10-07 15:18:05 +0200

    chore: create DefaultLayout

--- esao-frontend/src/App.jsx
+++ esao-frontend/src/App.jsx
@@ -1,16 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Records from "./pages/Records"
 import RecordDetails from "./pages/RecordDetails"
+import DefaultLayout from "./layouts/DefaultLayout"
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<h1>Pagina di Home</h1>} />
-        <Route path="/records" element={<Records />} />
-        <Route path="/records/:id" element={<RecordDetails />} />
-        <Route path="*" element={<h1>404 Not Found</h1>} />
+        <Route element={<DefaultLayout />}>
+          <Route path="/" element={<h1>Pagina di Home</h1>} />
+          <Route path="/records" element={<Records />} />
+          <Route path="/records/:id" element={<RecordDetails />} />
+          <Route path="*" element={<h1>404 Not Found</h1>} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
