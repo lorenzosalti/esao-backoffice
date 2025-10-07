cb8d1f60bbbcb00ce08ab09c2072fb8e2727d1ca
lorenzosalti <lorenzosalti@gmail.com>
2025-10-07 19:18:34 +0200

    chore: add isLoading state variable to context

--- esao-frontend/src/App.jsx
+++ esao-frontend/src/App.jsx
@@ -2,20 +2,29 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Records from "./pages/Records"
 import RecordDetails from "./pages/RecordDetails"
 import DefaultLayout from "./layouts/DefaultLayout"
+import GlobalContext from "./contexts/GlobalContext"
+import { useState } from "react"
 
 function App() {
 
+  const [isLoading, setIsLoading] = useState(false);
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<DefaultLayout />}>
-          <Route path="/" element={<h1>Pagina di Home</h1>} />
-          <Route path="/records" element={<Records />} />
-          <Route path="/records/:id" element={<RecordDetails />} />
-          <Route path="*" element={<h1>404 Not Found</h1>} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <GlobalContext.Provider value={{
+      isLoading,
+      setIsLoading
+    }}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<DefaultLayout />}>
+            <Route path="/" element={<h1>Pagina di Home</h1>} />
+            <Route path="/records" element={<Records />} />
+            <Route path="/records/:id" element={<RecordDetails />} />
+            <Route path="*" element={<h1>404 Not Found</h1>} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </GlobalContext.Provider>
   )
 }
 
