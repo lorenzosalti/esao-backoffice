9f3c47a0eb3a9e0265b4abcb716b80d5241f4183
lorenzosalti <lorenzosalti@gmail.com>
2025-10-07 15:17:43 +0200

    chore: create Footer component

---
+++ esao-frontend/src/components/Footer.jsx
@@ -0,0 +1,19 @@
+
+
+function Footer() {
+
+  return (
+    <footer class="bg-secondary-subtle p-5">
+      <h4>Footer</h4>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">Chi siamo</li>
+        <li class="list-group-item">A second item</li>
+        <li class="list-group-item">A third item</li>
+        <li class="list-group-item">A fourth item</li>
+        <li class="list-group-item">And a fifth one</li>
+      </ul>
+    </footer>
+  )
+}
+
+export default Footer  
