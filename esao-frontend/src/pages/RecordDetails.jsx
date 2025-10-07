1aa7c0dbb62bcf8df0dab80d1acc820125183062
lorenzosalti <lorenzosalti@gmail.com>
2025-10-07 11:48:54 +0200

    feat: create main pages

---
+++ esao-frontend/src/pages/RecordDetails.jsx
@@ -0,0 +1,12 @@
+
+function RecordDetails() {
+
+  return (
+    <>
+      <h1>Every Sound At Once!</h1>
+      <h2>Pagina di dettaglio del disco</h2>
+    </>
+  )
+}
+
+export default RecordDetails
---
+++ esao-frontend/src/pages/Records.jsx
@@ -0,0 +1,12 @@
+
+function Records() {
+
+  return (
+    <>
+      <h1>Every Sound At Once!</h1>
+      <h2>Pagina di lista dei dischi</h2>
+    </>
+  )
+}
+
+export default Records
