f2f7c8a662604afe7c5841a87efe6be14d52d5fe
lorenzosalti <lorenzosalti@gmail.com>
2025-10-07 11:49:17 +0200

    feat: create simple routing

--- esao-frontend/src/App.jsx
+++ esao-frontend/src/App.jsx
@@ -1,8 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Records from "./pages/Records"
+import RecordDetails from "./pages/RecordDetails"
 
 function App() {
 
   return (
-    <h1>Every Sound At Once!</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Pagina di Home</h1>} />
+        <Route path="/records" element={<Records />} />
+        <Route path="/records/:id" element={<RecordDetails />} />
+        <Route path="*" element={<h1>404 Not Found</h1>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
