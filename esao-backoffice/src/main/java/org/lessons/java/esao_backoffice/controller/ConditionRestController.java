4d5b5943335e9b506635a6bae408fb33e8aef989
lorenzosalti <lorenzosalti@gmail.com>
2025-10-08 11:12:55 +0200

    chore: create ConditionRestController

---
+++ esao-backoffice/src/main/java/org/lessons/java/esao_backoffice/controller/ConditionRestController.java
@@ -0,0 +1,12 @@
+package org.lessons.java.esao_backoffice.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/conditions")
+public class ConditionRestController {
+
+}
