8e2d5221a077ec34ebf5aac58d4cc77887cee0e8
lorenzosalti <lorenzosalti@gmail.com>
2025-10-08 11:19:12 +0200

    feat: add ConditionRestController index method

--- esao-backoffice/src/main/java/org/lessons/java/esao_backoffice/controller/ConditionRestController.java
+++ esao-backoffice/src/main/java/org/lessons/java/esao_backoffice/controller/ConditionRestController.java
@@ -1,5 +1,12 @@
 package org.lessons.java.esao_backoffice.controller;
 
+import java.util.List;
+
+import org.lessons.java.esao_backoffice.model.Condition;
+import org.lessons.java.esao_backoffice.service.ConditionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,4 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/conditions")
 public class ConditionRestController {
 
+  @Autowired
+  private ConditionService conditionService;
+
+  public ResponseEntity<List<Condition>> index() {
+    List<Condition> conditionListAttempt = conditionService.findAll();
+    if (conditionListAttempt.isEmpty()) {
+      return new ResponseEntity<List<Condition>>(HttpStatus.NOT_FOUND);
+    }
+    return new ResponseEntity<List<Condition>>(conditionListAttempt, HttpStatus.OK);
+  }
 }
